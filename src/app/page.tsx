a2ab3d1a60c930f236954b2d4ab6e71c82cc573b
neforward <141821349+neforward@users.noreply.github.com>
2024-04-12 17:40:44 +0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,191 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useEffect, useState } from "react";
+import { sanitize } from "dompurify";
+import InputMask from "react-input-mask";
+
+interface Review {
+  id: number;
+  text: string;
+}
+
+interface Product {
+  id: number;
+  image_url: string;
+  title: string;
+  description: string;
+  price: number;
+}
+
+interface CartItem {
+  product: Product;
+  quantity: number;
+}
 
 export default function Home() {
+  const [reviews, setReviews] = useState<Review[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [cartItems, setCartItems] = useState<CartItem[]>(
+    JSON.parse(localStorage.getItem("cartItems") || "[]") || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      const res = await fetch("http://o-complex.com:1337/reviews");
+      if (res.ok) {
+        const data = await res.json();
+        setReviews(data);
+      }
+    };
+
+    fetchReviews();
+  }, []);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch(
+        "http://o-complex.com:1337/products?page=1&page_size=20"
+      );
+      const data = await res.json();
+      const productsData = data.products || [];
+      setProducts(productsData);
+    };
+    fetchProducts();
+  }, []);
+
+  const addToCart = (product: Product) => {
+    const itemIndex = cartItems.findIndex(
+      (item) => item.product.id === product.id
+    );
+    if (itemIndex !== -1) {
+      const diffCartItems = [...cartItems];
+      diffCartItems[itemIndex].quantity++;
+      setCartItems(diffCartItems);
+    } else {
+      setCartItems([...cartItems, { product, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (productId: number) => {
+    const itemIndex = cartItems.findIndex(
+      (item) => item.product.id === productId
+    );
+    if (itemIndex !== -1) {
+      const diffCartItems = [...cartItems];
+      if (diffCartItems[itemIndex].quantity > 1) {
+        diffCartItems[itemIndex].quantity--;
+      } else {
+        diffCartItems.splice(itemIndex, 1);
+      }
+      setCartItems(diffCartItems);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="header-container">
+        <header className="header">
+          <h2>тестовое задание</h2>
+        </header>
+      </div>
+      <main className="main">
+        <div className="main-container">
+          <div className="main-content">
+            <div className="main-reviews">
+              {reviews.map((review, i) => (
+                <div className="review-box" key={i}>
+                  <h4>{sanitize(review.id.toString())}</h4>
+                  <p>{sanitize(review.text)}</p>
+                </div>
+              ))}
+            </div>
+            <div className="center">
+              <div className="adding-items">
+                <h2>Добавленные товары</h2>
+                {cartItems.map((item, index) => (
+                  <div className="row" key={index}>
+                    <h5>{sanitize(item.product.title)}</h5>
+                    <span>x{item.quantity}</span>
+                    <span>{item.product.price * item.quantity}₽</span>
+                  </div>
+                ))}
+                <div className="main-order">
+                  <InputMask
+                    mask="+9 (999) 999-99-99"
+                    placeholder="Введите номер телефона"
+                  />
+                  <button>заказать</button>
+                </div>
+              </div>
+            </div>
+            <div className="main-items">
+              <div className="container">
+                <div className="items-content">
+                  {products.map((product) => {
+                    const cartItem = cartItems.find(
+                      (item) => item.product.id === product.id
+                    );
+                    const quantityInCart = cartItem ? cartItem.quantity : 0;
+                    return (
+                      <div className="item-box" key={product.id}>
+                        <div className="center">
+                          <img src={product.image_url} alt="" />
+                        </div>
+                        <div className="des-n-title">
+                          <h2>{sanitize(product.title)}</h2>
+                          <p>{sanitize(product.description)}</p>
+                        </div>
+                        <div className="price">
+                          <h4>цена: {product.price}₽</h4>
+                        </div>
+                        <div className="center">
+                          {quantityInCart > 0 ? (
+                            <div className="plus-input-minus">
+                              <button
+                                onClick={() => removeFromCart(product.id)}
+                              >
+                                -
+                              </button>
+                              <input
+                                type="number"
+                                value={quantityInCart}
+                                onChange={(e) => {
+                                  const value = parseInt(e.target.value);
+                                  if (!isNaN(value) && value >= 0) {
+                                    const diffCartItems = [...cartItems];
+                                    const itemIndex = diffCartItems.findIndex(
+                                      (item) => item.product.id === product.id
+                                    );
+                                    if (itemIndex !== -1) {
+                                      diffCartItems[itemIndex].quantity = value;
+                                      setCartItems(diffCartItems);
+                                    }
+                                  }
+                                }}
+                              />
+
+                              <button onClick={() => addToCart(product)}>
+                                +
+                              </button>
+                            </div>
+                          ) : (
+                            <button onClick={() => addToCart(product)}>
+                              купить
+                            </button>
+                          )}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
